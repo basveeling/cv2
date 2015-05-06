0afef61d69bd931cacf5df8fba70829af2a02900
Peter Dekker <peter.dekker@student.uva.nl>
2015-05-06 12:59:27 +0200

    loaded and converted pointview matrix

--- Assignment3/ass3.py
+++ Assignment3/ass3.py
@@ -6,7 +6,25 @@ import numpy as np
 import cv2
 import pickle
 
+# Load pointview matrix (list) from file and convert to matrix
+# using trailing zeros
+def load_pointview_matrix(filename):
+    f = open(filename,"rb")
+    pointview_list = pickle.load(f)
+    longest_row = 0
+    for row in pointview_list:
+        row_length = len(row)
+        if row_length > longest_row:
+            longest_row = row_length
+    matrix = np.zeros((len(pointview_list),longest_row))
+    
+    for i,row in enumerate(pointview_list):
+        row_length = len(row)
+        matrix[i,:row_length] = row
+    matrix = np.nan_to_num(matrix)
+    return matrix
+
 if __name__ == '__main__':
-    f = open("../pointview.m","rb")
-    pointview_mtr = pickle.load(f)
-    print pointview_mtr
+    matr = load_pointview_matrix("../pointview.m")
+    print matr
+    #print(np.sum(pointview_mtr[:],axis=1)[0])
